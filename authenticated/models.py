0c0969c2306c95e33c4b3e2b9f8c9f8d63bef959
Uğur <uqrcan.01@gmail.com>
2024-04-01 12:40:39 +0300

    models.py added

--- authenticated/models.py
+++ authenticated/models.py
@@ -15,6 +15,19 @@ class UserManager(BaseUserManager):
         user.set_password(password)
         user.save(using=self._db)
         return user
+    
+    
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff = True')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser = True')
+
+        return self.create_user(email, password, **extra_fields)
 
 
 class UserData(AbstractUser):
@@ -23,6 +36,10 @@ class UserData(AbstractUser):
     name = models.CharField(max_length=100, unique=True)
     email = models.EmailField(max_length=100, unique=True)
     date_joined = models.DateTimeField(auto_now_add=True)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
     
     objects = UserManager()
     
--- stockApp/models.py
+++ stockApp/models.py
@@ -1,3 +1,68 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=40)
+
+    def _str_(self):
+        return self.name
+
+    def total_products(self):
+        return self.product_set.count()
+
+class Brand(models.Model):
+    name = models.CharField(max_length=40)
+    image = models.ImageField(upload_to='brands/')
+
+    def _str_(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=40)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    stock = models.IntegerField(editable=False) # read_only olarak işaretlenmiştir.
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def _str_(self):
+        return self.name
+
+class Firm(models.Model):
+    name = models.CharField(max_length=40)
+    phone = models.CharField(max_length=20)
+    address = models.TextField()
+    image = models.ImageField(upload_to='firms/')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def _str_(self):
+        return self.name
+
+class Purchase(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    firm = models.ForeignKey(Firm, on_delete=models.CASCADE)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price_total = models.DecimalField(max_digits=10, decimal_places=2, editable=False) # Hesaplanacak alan
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        self.price_total = self.quantity * self.price
+        super(Purchase, self).save(*args, **kwargs)
+
+class Sale(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price_total = models.DecimalField(max_digits=10, decimal_places=2, editable=False) # Hesaplanacak alan
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        self.price_total = self.quantity * self.price
+        super(Sale, self).save(*args, **kwargs)
